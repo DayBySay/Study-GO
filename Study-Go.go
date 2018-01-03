71f2393beb1841e38b8fe5b220f4bc36294205d6
Takayuki Sei <takayuki_sei@voyagegroup.com>
2018-01-03 23:04:08 +0900

    ...

--- Study-Go.go
+++ Study-Go.go
@@ -2,24 +2,19 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"runtime"
 )
 
-func Sqrt(x float64) (z float64) {
-	z = 1.0
-	for i := 0; i < 10; i++ {
-		x := z - (z*z-x)/(2*z)
-		if x == z {
-			return
-		}
-		z = x
-	}
-
-	return
-}
-
 func main() {
-	i := 5.0
-	fmt.Println(Sqrt(i))
-	fmt.Println(math.Sqrt(i))
+	fmt.Print("Go runs on ")
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("OS X.")
+	case "linux":
+		fmt.Println("Linux.")
+	default:
+		// freebsd, openbsd,
+		// plan9, windows...
+		fmt.Printf("%s.", os)
+	}
 }
