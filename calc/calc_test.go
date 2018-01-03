1b2fd4c82c051dde3c72ccf36931ce9ddf31f296
Takayuki Sei <takayuki_sei@voyagegroup.com>
2018-01-04 01:44:53 +0900

    ...

--- calc/calc_test.go
+++ calc/calc_test.go
@@ -2,10 +2,16 @@ package calc
 
 import (
     "testing"
+    "fmt"
 )
 
 func TestSum (t *testing.T) {
     if sum(1, 2) != 3 {
         t.Fatal("sum(1,2) should be 3, but doesn't match")
     }
+}
+
+func ExampleSum () {
+    fmt.Println(sum(1, 3))
+    // OutPut: 4
 }
