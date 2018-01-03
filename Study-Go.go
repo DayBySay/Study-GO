3d547f8b0fb2b85d46b7fa57658f21244b60c61b
Takayuki Sei <takayuki_sei@voyagegroup.com>
2018-01-03 22:21:25 +0900

    ...

--- Study-Go.go
+++ Study-Go.go
@@ -4,9 +4,10 @@ import (
 	"fmt"
 )
 
-var c, python, java bool
-
 func main() {
-	var i int
-	fmt.Println(i, c, python, java)
+	var i, j int = 1, 2
+	k := 3
+	var c, python, java = true, false, "no!"
+
+	fmt.Println(i, j, k, c, python, java)
 }
